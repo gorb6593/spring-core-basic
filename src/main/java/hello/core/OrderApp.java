054c72924d0a7b2ccf93af2364f3d5b3674c3c12
harry <gorb6593@gmail.com>
2025-01-01 14:30:02 +0900

    테스트코드 작성

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -14,9 +14,10 @@ public class OrderApp {
         OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
+        Member member = new Member(memberId,  "memberA", Grade.VIP);
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
     }
 }
