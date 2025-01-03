9e3ab49035eca1883c271bd09dc3bc8d227ba80d
harry <gorb6593@gmail.com>
2025-01-03 23:52:25 +0900

    의존성 주입 새로운 구조로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,10 +10,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+    }
+
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static FixDiscountPolicy getDiscountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
